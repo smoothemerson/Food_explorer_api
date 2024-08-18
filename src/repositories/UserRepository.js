d52f5467803b626fa974b9b9924887edb9831028
emerson <emerson8096@gmail.com>
2024-08-18 19:44:27 -0300

    feat: add findByIdAndVerifications

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,4 +1,5 @@
 const sqliteConnection = require("../database/sqlite");
+const AppError = require("../utils/AppError");
 
 class UserRepository {
   async findByEmail(email) {
@@ -19,6 +20,51 @@ class UserRepository {
     );
 
     return { id: UserId };
+  }
+
+  async findByIdAndVerifications(user_id, name, email, password, old_password) {
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ]);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    return user;
+  }
+
+  async update({ user_id }) {
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ]);
+
+    await database.run(
+      `
+      UPDATE users SET 
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, user_id]
+    );
+
+    return;
   }
 }
 
