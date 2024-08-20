6f58e91603f8ace71cfae93d8ef0a095be4c0671
emerson <emerson8096@gmail.com>
2024-08-20 08:02:45 -0300

    fix: remove resolver to resolve

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -3,7 +3,7 @@ const path = require("path");
 const crypto = require("crypto");
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
-const UPLOADS_FOLDER = path.resolver(TMP_FOLDER, "uploads");
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 const MULTER = {
   storage: multer.diskStorage({
