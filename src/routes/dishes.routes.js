28da96c2c8f0be46dac866646413f09822910a45
emerson <emerson8096@gmail.com>
2024-08-20 08:19:32 -0300

    feat: add dishes routes

---
+++ src/routes/dishes.routes.js
@@ -0,0 +1,22 @@
+const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
+
+const DishesController = require("../controllers/DishesController")
+const DishesImageController = require("../controllers/DishesImageController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
+const dishesRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
+
+const dishesController = new DishesController()
+const dishesImageController = new DishesImageController()
+
+dishesRoutes.use(ensureAuthenticated)
+
+dishesRoutes.post("/", dishesController.create)
+dishesRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), dishesImageController.update)
+dishesRoutes.get("/:id", dishesController.show)
+dishesRoutes.delete("/:id", dishesController.delete)
+
+module.exports = dishesRoutes
