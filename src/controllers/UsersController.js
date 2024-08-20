550669cab7b528b7de248fa2e4657e37712e87df
emerson <emerson8096@gmail.com>
2024-08-20 07:31:45 -0300

    feat: add update user function

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -21,18 +21,47 @@ class UsersController {
     const { name, email, password, old_password } = request.body;
     const user_id = request.user.id;
 
-    const userRepository = new UserRepository();
-    const userUpdateService = new userUpdateService(userRepository);
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id])
 
-    await userUpdateService.execute({
-      user_id,
-      name,
-      email,
-      password,
-      old_password,
-    });
+    if (!user) {
+      throw new AppError("Usuário não encontrado")
+    }
 
-    return response.json();
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso")
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password) {
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere")
+      }
+
+      user.password = await hash(password, parseInt(8, 10))
+    }
+
+    await database.run(`
+      UPDATE users SET 
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, user_id]
+    )
+
+    return response.json()
   }
 }
 
