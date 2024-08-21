8826b2886dfff903694549571d80cbca34405563
emerson <emerson8096@gmail.com>
2024-08-21 09:03:33 -0300

    fix: remove extra comma

--- src/database/sqlite/migrations/createUsers.js
+++ src/database/sqlite/migrations/createUsers.js
@@ -5,7 +5,7 @@ const createUsers = `
     email VARCHAR,
     password VARCHAR,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   )
 `;
 
