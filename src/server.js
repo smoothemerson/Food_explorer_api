58a2788540e4633815f2c36f425983e7bc6606a8
emerson <emerson8096@gmail.com>
2024-08-23 11:13:25 -0300

    feat: add cors configs

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 require("express-async-errors")
 require("dotenv/config")
 
-const AppError = require("./utils/AppError")
-const uploadConfig = require("./configs/upload")
 const cookieParser = require("cookie-parser")
-
 const cors = require("cors")
 const express = require("express");
 const routes = require("./routes")
 
+const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
+
 const app = express();
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  credentials: true
+}));
 app.use(cookieParser())
-app.use(cors())
 app.use(express.json())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
